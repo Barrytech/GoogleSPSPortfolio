6ef3f7b4c124387c54ae25dc8082f70fd07377ef
Abdoul Barry <abarry@sps-program.com>
2020-06-17 16:12:34 -0400

    testing new way of doing things - doPost

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,18 +22,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import com.google.gson.Gson;
+import java.io.PrintWriter;
+ 
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+   
     private ArrayList<String> facts;
     public void init() {
-   facts = new ArrayList<String>();
+        facts = new ArrayList<String>();
 
       facts.add(" I was born and raised in Guinea, West-Africa.");
       facts.add(" I have never had McDonal's.");
       facts.add(" I will probably graduate in my junior year.");
     }
+
+
+
+
      
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -55,34 +63,64 @@ public class DataServlet extends HttpServlet {
 }
 
  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the input from the form.
-    String text = getParameter(request, "text-input", "");
-    boolean upperCase = Boolean.parseBoolean(getParameter(request, "upper-case", "false"));
-    boolean sort = Boolean.parseBoolean(getParameter(request, "sort", "false"));
+      // read form fields
+        String firstname = request.getParameter("first-name-field");
+        String lastname = request.getParameter("last-name-field");
+        String email = request.getParameter("email-field");
+        String comments = request.getParameter("feedback");
+         
+        System.out.println("First name: " + firstname);
+        System.out.println("Last name: " + lastname);
+        System.out.println("Email: " + email);
+        System.out.println("Comments: " + comments);
+ 
+        // do some processing here...
+         
+        // get response writer
+        PrintWriter writer = response.getWriter();
+         
+        // build HTML code
+        String htmlRespone = "<html>";
+        htmlRespone += "<h2> First-Name is: " + firstname + "<br/>";      
+        htmlRespone += " Last Name is: " + lastname + "<br/>";
+        htmlRespone += " Email address is: " + lastname + "<br/>";
+        htmlRespone += " Comment is: " + lastname + "<h2>"; 
+        htmlRespone += "</html>";
+         
+        // return response
+        writer.println(htmlRespone);
 
-    // Convert the text to upper case.
-    if (upperCase) {
-      text = text.toUpperCase();
-    }
+ }
 
-    // Break the text into individual words.
-    String[] words = text.split("\\s*,\\s*");
+//       System.out.println(" inside doPost");
+//     // Get the input from the form.
+//     String text = getParameter(request, "text-input", "");
+//     boolean upperCase = Boolean.parseBoolean(getParameter(request, "upper-case", "false"));
+//     boolean sort = Boolean.parseBoolean(getParameter(request, "sort", "false"));
 
-    // Sort the words.
-    if (sort) {
-      Arrays.sort(words);
-    }
+//     // Convert the text to upper case.
+//     if (upperCase) {
+//       text = text.toUpperCase();
+//     }
 
-    // Respond with the result.
-    response.setContentType("text/html;");
-    response.getWriter().println(Arrays.toString(words));
-  }
-   private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    if (value == null) {
-      return defaultValue;
-    }
-    return value;
-  }
+//     // Break the text into individual words.
+//     String[] words = text.split("\\s*,\\s*");
+
+//     // Sort the words.
+//     if (sort) {
+//       Arrays.sort(words);
+//     }
+
+//     // Respond with the result.
+//     response.setContentType("text/html;");
+//     response.getWriter().println(Arrays.toString(words));
+//   }
+//    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+//     String value = request.getParameter(name);
+//     if (value == null) {
+//       return defaultValue;
+//     }
+//     return value;
+ 
 }
 
